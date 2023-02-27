e1bd5a5b6fe73ce8831f0e5093f2bdc4b81cc366
Bhanu Pratap Reddy <Bhanu.devopss@gmail.com>
2023-02-27 11:26:48 +0530

    Update learn-terraform/

--- 04-vars/var.tf
+++ 04-vars/var.tf
@@ -46,3 +46,19 @@ output "types" {
   value = "variable sample5 - ${var.sample5}, First value in list - ${var.sample6[0]}, boolean value of Map = ${var.sample7["boolean"]}"
 }
 
+variable "d1" {
+  default = [
+    {
+      course_name = "aws"
+      trainer_name = "raju"
+    },
+    {
+      course_name = "devops"
+      trainer_name = "ravi"
+    }
+  ]
+}
+
+output "course_names" {
+  value = var.d1.*.course_name
+}
