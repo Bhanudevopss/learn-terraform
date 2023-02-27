4bb86baba5baf78c9f5ef54eab32d6dd47f2f156
Bhanu Pratap Reddy <Bhanu.devopss@gmail.com>
2023-02-27 17:07:52 +0530

    Update learn-terraform/

---
+++ roboshop-setup/ex3/main.tf
@@ -0,0 +1,30 @@
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+
+resource "aws_instance" "instances" {
+  for_each = var.instances
+  ami = "ami-0a017d8ceb274537d"
+  instance_type = each.value["type"]
+  vpc_security_group_ids = ["sg-07d70a19582692526"]
+  tags = {
+    Name = each.value["name"]
+  }
+}
+
+variable "instances" {
+  default = {
+    catalogue = {
+      name = "catalogue"
+      type = "t3.micro"
+    }
+    user = {
+      name = "user"
+      type = "t3.small"
+    }
+  }
+}
+
