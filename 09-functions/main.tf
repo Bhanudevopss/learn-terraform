f5aafcf7ac1589f0b354880a72c0c94045fc09a1
Bhanu Pratap Reddy <Bhanu.devopss@gmail.com>
2023-03-02 16:14:23 +0530

    Update learn-terraform/

---
+++ 09-functions/main.tf
@@ -0,0 +1,15 @@
+variable "test" {
+  default = {
+    "catalogue" = {
+      "private_ip" = "172.31.10.172"
+    }
+    "user" = {
+      "private_ip" = "172.31.9.24"
+    }
+  }
+}
+
+output "test" {
+  value = var.test
+}
+
