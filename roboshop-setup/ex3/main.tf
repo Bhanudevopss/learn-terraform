6f5f94a342e59dd1b907989675363d156338c4ae
Bhanu Pratap Reddy <Bhanu.devopss@gmail.com>
2023-03-02 11:09:14 +0530

    Update learn-terraform/

--- roboshop-setup/ex3/main.tf
+++ roboshop-setup/ex3/main.tf
@@ -28,3 +28,45 @@ variable "instances" {
   }
 }
 
+## Immature code
+//variable "names" {
+//  default = [ "catalogue", "user"]
+//}
+//
+//variable "types" {
+//  default = [ "t3.micro", "t3.small"]
+//}
+//
+//resource "aws_instance" "instances" {
+//  count = length(var.names)
+//  ami = "ami-0a017d8ceb274537d"
+//  instance_type = var.types[count.index]
+//  vpc_security_group_ids = ["sg-07d70a19582692526"]
+//  tags = {
+//    Name = var.names[count.index]
+//  }
+//}
+
+## Little mature code
+variable "demo" {
+  default = [
+    {
+      name = "catalogue"
+      type = "t3.micro"
+    },
+    {
+      name = "user"
+      type = "t3.small"
+    }
+  ]
+}
+
+resource "aws_instance" "instances" {
+  count = length(var.demo)
+  ami = "ami-0a017d8ceb274537d"
+  instance_type = var.demo[count.index]["type"]
+  vpc_security_group_ids = ["sg-07d70a19582692526"]
+  tags = {
+    Name = var.demo[count.index]["name"]
+  }
+}
