a25a4b31fc03125b8a6d7aae809fb839a0e28e43
Bhanu Pratap Reddy <Bhanu.devopss@gmail.com>
2023-03-12 00:36:46 +0530

    Update learn-terraform/

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -2,6 +2,7 @@ resource "aws_spot_instance_request" "node1" {
   ami                    = "ami-07d0eaa2d077d21b9"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-07d70a19582692526"]
+  wait_for_fulfillment   = true
 
   tags = {
     Name = "prom-test-server"
@@ -18,6 +19,7 @@ resource "aws_spot_instance_request" "node2" {
   ami                    = "ami-07d0eaa2d077d21b9"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-07d70a19582692526"]
+  wait_for_fulfillment   = true
 
   tags = {
     Name = "prom-test-node"
