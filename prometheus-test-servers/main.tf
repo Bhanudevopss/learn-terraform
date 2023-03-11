6644d705eaee04152dff02b0125d6709ad69915c
Bhanu Pratap Reddy <Bhanu.devopss@gmail.com>
2023-03-12 00:11:11 +0530

    Update learn-terraform/

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -1,8 +1,8 @@
 resource "aws_spot_instance_request" "node1" {
-  ami           = "ami-07d0eaa2d077d21b9"
-  instance_type = "t3.micro"
+  ami                    = "ami-07d0eaa2d077d21b9"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-07d70a19582692526"]
-  wait_for_fulfillment = true
+  wait_for_fulfillment   = true
 
   tags = {
     Name = "prom-test-server"
@@ -10,10 +10,10 @@ resource "aws_spot_instance_request" "node1" {
 }
 
 resource "aws_spot_instance_request" "node2" {
-  ami           = "ami-07d0eaa2d077d21b9"
-  instance_type = "t3.micro"
+  ami                    = "ami-07d0eaa2d077d21b9"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-07d70a19582692526"]
-  wait_for_fulfillment = true
+  wait_for_fulfillment   = true
 
   tags = {
     Name = "prom-test-node"
